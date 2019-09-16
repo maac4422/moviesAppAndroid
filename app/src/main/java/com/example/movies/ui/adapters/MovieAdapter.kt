41096a6eee6c61580ea9da4c66ee2b528a28da71
Miguel Angel Arenas Correa <miguel.arenas@Miguel-Arenas-5.local>
2019-09-16 15:51:00 -0500

    Binding movie adapter

--- app/src/main/java/com/example/movies/ui/adapters/MovieAdapter.kt
+++ app/src/main/java/com/example/movies/ui/adapters/MovieAdapter.kt
@@ -1,12 +1,40 @@
 package com.example.movies.ui.adapters
 
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
-/*
+import com.example.movies.R
+import com.example.movies.databinding.ItemMovieBinding
+import com.example.movies.domain.entities.Movie
+
 class MovieAdapter(private  val context: Context) : RecyclerView.Adapter<MovieAdapter.MovieViewHolder>() {
 
-    class MovieViewHolder : RecyclerView.ViewHolder() {
+    private var movies: List<Movie> = listOf()
 
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
+        val layoutInflater = LayoutInflater.from(context)
+        val binding: ItemMovieBinding = DataBindingUtil.inflate(layoutInflater, R.layout.item_movie, parent, false)
+        return MovieViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int {
+        return movies.size
+    }
+
+    override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
+        holder?.bind(movies[position])
+    }
+
+    fun updateMovie(movies: List<Movie>) {
+        this.movies = movies
+        notifyDataSetChanged()
+    }
+
+    class MovieViewHolder(private val binding: ItemMovieBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(movie: Movie) {
+
+        }
     }
 }
-*/
